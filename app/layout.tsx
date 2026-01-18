2359d265941566703718586504921aa842620f6a
CeerDecy <1748788674@qq.com>
2026-01-18 16:55:27 +0800

    feat: microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,15 @@ export default function RootLayout({
             gtag('config', 'G-7F8ZWNEZEK');
                 `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "v3ao233xyb");
+          `}
+        </Script>
       </head>
       <body
         className={`${spaceGrotesk.variable} ${bebasNeue.variable} antialiased`}
